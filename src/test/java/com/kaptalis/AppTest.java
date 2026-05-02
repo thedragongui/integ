fe03a7800affdc2a606a4b6d34bad10e689500ce
pvalle <phivalle@gmail.com>
2026-05-02 17:51:52 +0000

    introduction erreur dans les tests

--- src/test/java/com/kaptalis/AppTest.java
+++ src/test/java/com/kaptalis/AppTest.java
@@ -1,6 +1,6 @@
 package com.kaptalis;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +8,12 @@ import org.junit.jupiter.api.Test;
 public class AppTest {
     @Test
     public void testTemperatureCorrect() {
-        assertTrue(App.isTemperatureValid(5.0));
+        //assertTrue(App.isTemperatureValid(5.0));
+        
+        // ICI : On attend que 5.0 soit VALIDE (true), 
+        // mais on demande à JUnit de vérifier que c'est FAUX (assertFalse).
+        // Cela va provoquer une erreur de test.
+        assertFalse(App.isTemperatureValid(5.0));
     }
 
 
