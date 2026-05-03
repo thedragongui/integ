02e865f5a4dfb52bdd1f30642c227c9b2af553d8
pvalle <phivalle@gmail.com>
2026-05-03 09:01:23 +0000

    test PMD : supprime la variable inutilisée

--- src/main/java/com/kaptalis/App.java
+++ src/main/java/com/kaptalis/App.java
@@ -4,9 +4,9 @@ public class App {
 
     // PMD va détecter cette variable car elle est privée 
     // et n'est lue nulle part dans le code.
-    private static int unusedVariable = 42;
+    //private static int unusedVariable = 42;
 
-    
+
        public static void main(String[] args) {
         System.out.println("Kaptalis Monitoring System - Active");
     }
